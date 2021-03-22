f1bfdb3b92d04bb4e70b97ae84e69c15d4ff637b
Liang813 <62755766+Liang813@users.noreply.github.com>
2021-03-22 16:25:55 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,29 +1,29 @@
 import tensorflow_probability as tfp
 import tensorflow as tf
 
-try:
-    tfd = tfp.distributions
-    tfl = tf.linalg
+
+tfd = tfp.distributions
+tfl = tf.linalg
 
 
-    num_dims = 3
-    num_timesteps = 1
-    step_std = 1.0
-    noise_std = 5.0
+num_dims = 3
+num_timesteps = 1
+step_std = 1.0
+noise_std = 5.0
 
-    ssm = tfd.LinearGaussianStateSpaceModel(
-        num_timesteps=num_timesteps,
-        transition_matrix=tfl.LinearOperatorIdentity(num_dims),
-        transition_noise=tfd.MultivariateNormalDiag(
-            scale_diag=tf.fill([num_dims], tf.square(step_std))
-        ),
-        observation_matrix=tfl.LinearOperatorIdentity(num_dims),
-        observation_noise=tfd.MultivariateNormalDiag(
-            scale_diag=tf.fill([num_dims], tf.square(noise_std))
-        ),
-        initial_state_prior=tfd.MultivariateNormalDiag(scale_diag=tf.ones([num_dims])),
-    )
+ssm = tfd.LinearGaussianStateSpaceModel(
+    num_timesteps=num_timesteps,
+    transition_matrix=tfl.LinearOperatorIdentity(num_dims),
+    transition_noise=tfd.MultivariateNormalDiag(
+        scale_diag=tf.fill([num_dims], tf.square(step_std))
+    ),
+    observation_matrix=tfl.LinearOperatorIdentity(num_dims),
+    observation_noise=tfd.MultivariateNormalDiag(
+        scale_diag=tf.fill([num_dims], tf.square(noise_std))
+    ),
+    initial_state_prior=tfd.MultivariateNormalDiag(scale_diag=tf.ones([num_dims])),
+)
 
-    sample = ssm.sample()
-except Exception as e:
-    print(str(e))
+sample = ssm.sample()
+print("success")
+
