3bc9808a9ab0b13edba841c7300e33428413a0ba
Liang813 <62755766+Liang813@users.noreply.github.com>
2021-05-29 22:49:01 +0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,7 @@
+
+d = tfd.RelaxedBernoulli(0.01, probs=tf.ones(1)*.5)
+iid = tfd.Independent(d, reinterpreted_batch_ndims=1)
+
+iid.log_prob(iid.sample()) # <-- usually given nan
+
+iid.log_prob(d.sample()) # always gives the right result
