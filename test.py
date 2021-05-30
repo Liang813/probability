5aa92d1aef0594b41f574f2e650871e968275d4c
Liang813 <62755766+Liang813@users.noreply.github.com>
2021-05-30 15:04:44 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,12 @@
 
+import tensorflow_probability as tfp
+import tensorflow as tf
+tfd = tfp.distributions
+
 d = tfd.RelaxedBernoulli(0.01, probs=tf.ones(1)*.5)
 iid = tfd.Independent(d, reinterpreted_batch_ndims=1)
 
 iid.log_prob(iid.sample()) # <-- usually given nan
-
+print(iid.log_prob(iid.sample()))
 iid.log_prob(d.sample()) # always gives the right result
+print(iid.log_prob(d.sample()))
